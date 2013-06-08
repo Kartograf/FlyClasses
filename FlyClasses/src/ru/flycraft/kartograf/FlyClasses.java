02cffd1b4d09c830143c0aa18dfdad2b5725a260
Kartograf <kartogrof@gmail.com>
2013-06-08 21:13:43 +0400

    Test1

---
+++ FlyClasses/src/ru/flycraft/kartograf/FlyClasses.java
@@ -0,0 +1,13 @@
+package ru.flycraft.kartograf;
+
+public class FlyClasses {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
